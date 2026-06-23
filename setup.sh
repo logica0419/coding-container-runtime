9c7b1a57e0c73c2d6799397c40a640714395005a
Takuto Nagami <logica0419@gmail.com>
2026-06-24 01:16:48 +0900

    fix setup script
    Signed-off-by: Takuto Nagami <logica0419@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -eu
 
 repo_url=https://github.com/logica0419/coding-container-runtime.git
-workspace_dir=$(pwd)
 tmp_dir=$(mktemp -d)
 
 trap 'rm -rf "$tmp_dir"' EXIT INT TERM HUP
@@ -15,7 +14,7 @@ done
 
 git clone --depth 1 --filter=blob:none --sparse "$repo_url" "$tmp_dir/repo"
 git -C "$tmp_dir/repo" sparse-checkout set templates
-cp -a "$tmp_dir/repo/templates/." "$workspace_dir/"
+cp -a "$tmp_dir/repo/templates" .
 
 make init
 make rootfs
